e457797d0d4ca9f8676cf264ba7c0d805e0248d2
swimmingkiim <swimmingkiim@gmail.com>
2022-03-13 13:40:39 +0900

    remove : not used import

--- lib/src/resource/api/service.dart
+++ lib/src/resource/api/service.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../config/config.dart';
