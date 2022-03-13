b47e9b01d6349e8799a67c234e102d3a024f671f
swimmingkiim <swimmingkiim@gmail.com>
2022-03-13 13:40:14 +0900

    add : Payment initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
+import 'src/resource/payment/service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
+  // PaymentService.instance.initialize();
   runApp(const FlutterAppStarterApp());
 }
 
